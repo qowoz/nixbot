553cc5c488d95aba7418244fbcb6563fdf215759
zowoq <59103226+zowoq@users.noreply.github.com>
2026-08-27 11:11:22 +1000

    Revert "packages/nix-eval-jobs: build against Nix 2.35 components"
    This reverts commit fa5fe005003053f275d0a05fe10857f04c5a68f6.

--- packages/nix-eval-jobs.nix
+++ packages/nix-eval-jobs.nix
@@ -1,17 +1,13 @@
 # TODO: drop when nixpkgs ships nix-eval-jobs >= 2.35.1
 { pkgs, fetchFromGitHub }:
-(pkgs.nix-eval-jobs.override {
-  # nix-eval-jobs 2.35.x requires Nix >= 2.35 (tryEnterPrivateMountNamespace)
-  nixComponents = pkgs.nixVersions.nixComponents_2_35;
-}).overrideAttrs
-  (
-    finalAttrs: _prevAttrs: {
-      version = "2.35.1";
-      src = fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nix-eval-jobs";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-EFJnN35L7UieL8zV8qPrpqfdfzztWksY8JYuXF+mr9o=";
-      };
-    }
-  )
+pkgs.nix-eval-jobs.overrideAttrs (
+  finalAttrs: _prevAttrs: {
+    version = "2.35.1";
+    src = fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nix-eval-jobs";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-EFJnN35L7UieL8zV8qPrpqfdfzztWksY8JYuXF+mr9o=";
+    };
+  }
+)
