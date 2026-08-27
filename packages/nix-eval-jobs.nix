0433d99b835e7308139894c4faa14b80d4991ab1
zowoq <59103226+zowoq@users.noreply.github.com>
2026-08-27 11:10:45 +1000

    Revert "nix-eval-jobs: backport nix 2.35 for 26.05"
    This reverts commit df8d4b65ae9007514235e16f727e36a793733369.

--- packages/nix-eval-jobs.nix
+++ packages/nix-eval-jobs.nix
@@ -1,48 +1,8 @@
 # TODO: drop when nixpkgs ships nix-eval-jobs >= 2.35.1
 { pkgs, fetchFromGitHub }:
-let
-  # boost.context fixes needed by nix 2.35 coroutines (NixOS/nix#16174)
-  boostPatched =
-    if
-      pkgs.lib.versionAtLeast pkgs.boost.version "1.88" && pkgs.lib.versionOlder pkgs.boost.version "1.93"
-    then
-      pkgs.boost.overrideAttrs (prevAttrs: {
-        patches = (prevAttrs.patches or [ ]) ++ [
-          (pkgs.fetchpatch {
-            url = "https://github.com/boostorg/context/commit/0921b9fd5c776aec7748475c6c10807e0d51bc6d.patch";
-            relative = "include";
-            hash = "sha256-nQYMd3HFsDLxijnGdyas0ZHs3ylQVMGQL14K7F6MkF0=";
-          })
-          (pkgs.fetchpatch {
-            url = "https://github.com/boostorg/context/commit/5883212311535a0046031d74d1568ae173c1e35b.patch";
-            relative = "include";
-            hash = "sha256-CytNLi2d0wjI/lY5lDv98mwwQaEt7qeIs4UkE6QgCBU=";
-          })
-        ];
-      })
-    else
-      pkgs.boost;
-
-  # polyfill for nixpkgs without nix 2.35 (e.g. stable release branches)
-  nixComponents_2_35 =
-    pkgs.nixVersions.nixComponents_2_35 or (
-      (pkgs.nixVersions.nixComponents_2_34.overrideSource (fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nix";
-        tag = "2.35.2";
-        hash = "sha256-C/YEm/5IPiAMxQH5aHlkwgQMkLqK7NVsudEWdlzBZAA=";
-      })).overrideScope
-      (
-        _finalScope: _prevScope: {
-          version = "2.35.2";
-          boost = boostPatched;
-        }
-      )
-    );
-in
 (pkgs.nix-eval-jobs.override {
   # nix-eval-jobs 2.35.x requires Nix >= 2.35 (tryEnterPrivateMountNamespace)
-  nixComponents = nixComponents_2_35;
+  nixComponents = pkgs.nixVersions.nixComponents_2_35;
 }).overrideAttrs
   (
     finalAttrs: _prevAttrs: {
